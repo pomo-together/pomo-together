282bdd60326c49fca7eec142fe614a289c67e1b9
Joowan Kim <rlawndhks217@gmail.com>
2021-12-11 21:18:58 +0900

    Add SpringBoot settings

---
+++ src/main/java/pomo/joowan/pomotogether/PomoTogetherApplication.java
@@ -0,0 +1,13 @@
+package pomo.joowan.pomotogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomoTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomoTogetherApplication.class, args);
+	}
+
+}
