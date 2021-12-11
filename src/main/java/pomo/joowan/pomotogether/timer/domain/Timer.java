39f8bb8e89b3e7bf97955e6004b4638ba0da2fcc
Joowan Kim <rlawndhks217@gmail.com>
2021-12-11 21:37:26 +0900

    Add Timer pause feature

--- src/main/java/pomo/joowan/pomotogether/timer/domain/Timer.java
+++ src/main/java/pomo/joowan/pomotogether/timer/domain/Timer.java
@@ -1,13 +1,14 @@
 package pomo.joowan.pomotogether.timer.domain;
 
 import pomo.joowan.pomotogether.timer.exceptions.TimerIsAlreadyStartedException;
+import pomo.joowan.pomotogether.timer.exceptions.TimerIsAlreadyStoppedException;
 import pomo.joowan.pomotogether.timer.utils.Clock;
-import pomo.joowan.pomotogether.timer.utils.JavaClock;
 
 public class Timer {
+    private final Clock clock;
+
     private long startTime;
     private long elapsedSeconds;
-    private final Clock clock;
 
     public Timer(Clock clock) {
         this.clock = clock;
@@ -24,5 +25,13 @@ public class Timer {
             throw new TimerIsAlreadyStartedException();
         }
         this.startTime = clock.current();
+    }
+
+    public void pause() {
+        if (this.startTime == 0) {
+            throw new TimerIsAlreadyStoppedException();
+        }
+        this.elapsedSeconds = this.clock.current() - this.startTime;
+        this.startTime = 0;
     }
 }
--- src/test/java/pomo/joowan/pomotogether/timer/domain/DescribeTimer.java
+++ src/test/java/pomo/joowan/pomotogether/timer/domain/DescribeTimer.java
@@ -2,6 +2,7 @@ package pomo.joowan.pomotogether.timer.domain;
 
 import org.junit.jupiter.api.*;
 import pomo.joowan.pomotogether.timer.exceptions.TimerIsAlreadyStartedException;
+import pomo.joowan.pomotogether.timer.exceptions.TimerIsAlreadyStoppedException;
 import pomo.joowan.pomotogether.timer.utils.Clock;
 import pomo.joowan.pomotogether.timer.utils.JavaClock;
 
@@ -56,9 +57,55 @@ class DescribeTimer {
             }
 
             @Test
-            @DisplayName("타이머의 시작시각이 현재시각보다 빨라야 한다")
+            @DisplayName("타이머의 시작시각이 현재시각보다 이르거나 같아야 한다")
             void ItHasEarlierStartTimeThanCurrentTime() {
-                assertThat(timer.getStartTime()).isLessThan(clock.current());
+                assertThat(timer.getStartTime()).isLessThanOrEqualTo(clock.current());
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("pause 메소드는")
+    class DescribePause {
+        private Timer timer;
+
+        @Nested
+        @DisplayName("타이머가 동작하고 있을 때")
+        class ContextWhenTimerIsWorking {
+            @BeforeEach
+            void startTimer() {
+                timer = new Timer(clock);
+                timer.start();
+            }
+
+            @Test
+            @DisplayName("시작시각을 0으로 수정한다")
+            void ItUpdatesStartTimeAsZero() {
+                timer.pause();
+                assertThat(timer.getStartTime()).isEqualTo(0);
+            }
+
+            @Test
+            @DisplayName("경과시간을 현재시각과 시작시각의 차이로 수정한다")
+            void ItUpdatesElapsedTimeAsDifferenceBetweenStartTimeAndCurrentTime() {
+                timer.pause();
+                assertThat(timer.getStartTime()).isGreaterThanOrEqualTo(0);
+            }
+        }
+
+        @Nested
+        @DisplayName("타이머가 멈춰있을 때")
+        class ContextWhenTimerIsStopped {
+            @BeforeEach
+            void setUpTimer() {
+                timer = new Timer(clock);
+            }
+
+            @Test
+            @DisplayName("TimerIsAlreadyStoppedException을 던진다")
+            void ItThrowsTimerIsAlreadyStoppedException() {
+                assertThatThrownBy(() -> timer.pause())
+                        .isInstanceOf(TimerIsAlreadyStoppedException.class);
             }
         }
     }
